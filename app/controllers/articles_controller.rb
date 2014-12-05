df132b41310a191ff88c142bfc072f8ba93bd8ec
Harley <hdavis0090@gmail.com>
2014-12-05 15:39:58 -0800

    Tutorial completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
-  def new
-  end
+
+def new
+  @article = Article.new
+end
  
 def create
   @article = Article.new(article_params)
  
-  @article.save
-  redirect_to @article
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
 end
  
 def show
@@ -16,6 +21,35 @@ end
 def index
   @articles = Article.all
 end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
+
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+#authentication
+  http_basic_authenticate_with name: "cat", password: "meow", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+
 
 private
   def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +54,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :articles do
+    resources :comments
+  end
 end
